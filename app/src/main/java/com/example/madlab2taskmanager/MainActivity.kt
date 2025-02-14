ba069b4c26b895802f73fd2e4bd9c2026fd4026a
zrock44 <zrock44@tutanota.com>
2025-02-14 00:11:13 -0500

    Implemented the text box in the TaskInput composable. The composable takes in its properties as parameters to allow for reuse, though that isn't needed for the assignment. State for the text box is hoisted up through TaskInputField to MainScreen.

--- app/src/main/java/com/example/madlab2taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/madlab2taskmanager/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.madlab2taskmanager
 
+import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
+import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.madlab2taskmanager.ui.theme.MADLab2TaskManagerTheme
 
@@ -19,26 +37,90 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MADLab2TaskManagerTheme {
-                Greeting(
-                    name = "Android"
-                )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    var inputText by remember { mutableStateOf("") }
+
+    Column {
+        TaskInputField(
+            value = inputText,
+            onValueChange = { inputText = it }
+        )
+    }
+}
+
+@Composable
+fun TaskInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        TaskInput(
+            label = R.string.input_label,
+            leadingIcon = Icons.Filled.Create,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            value = value,
+            onValueChange = onValueChange
+        )
+    }
+}
+
+@Composable
+fun TaskInput(
+    @StringRes label: Int,
+    leadingIcon: ImageVector,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = keyboardOptions,
+        leadingIcon = {
+            Icon(
+                imageVector = leadingIcon,
+                contentDescription = null
+            )
+        },
+        label = {
+            Text(
+                text = stringResource(label)
+            )
+        },
         modifier = modifier
     )
+}
+
+@Composable
+fun TaskList(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun TaskItem(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MADLab2TaskManagerTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
