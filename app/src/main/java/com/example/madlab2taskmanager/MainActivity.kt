0b182d3a985ad02adf2f66558b159e79b92c9dac
zrock44 <zrock44@tutanota.com>
2025-02-16 18:13:09 -0500

    TaskButton has been added. It takes in the icon, click method, and enabled state as parameters for easy reuse (even though that won't be needed). The colors of the button will be updated later. TaskInputField now shows the button that will add the new task to the list and the TaskInput and TaskButton are aligned vertically. Added task data class that will be used for each task that goes in the list. It currently has task name and task completed state as properties. Implemented the TaskItem composable for the most part. It displays tasks in a way that I think looks fine. The composable takes in a task item. Renamed the preview composable. TaskItem is called in MainScreen composable right now, that is only for testing purposes.

--- app/src/main/java/com/example/madlab2taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/madlab2taskmanager/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.madlab2taskmanager
 
-import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
-import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,12 +30,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.madlab2taskmanager.data.Task
 import com.example.madlab2taskmanager.ui.theme.MADLab2TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,6 +66,10 @@ fun MainScreen(modifier: Modifier = Modifier) {
             value = inputText,
             onValueChange = { inputText = it }
         )
+        TaskItem(
+            task = Task(name = "Test task", completed = false),
+            onClick = {},
+        )
     }
 }
 
@@ -65,7 +79,10 @@ fun TaskInputField(
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(modifier = modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
         TaskInput(
             label = R.string.input_label,
             leadingIcon = Icons.Filled.Create,
@@ -74,6 +91,12 @@ fun TaskInputField(
             ),
             value = value,
             onValueChange = onValueChange
+        )
+        TaskButton(
+            // MAKE THE ICON DYNAMIC DEPENDING ON STATE OF ENABLED//////////////////////////////////
+            icon = Icons.Filled.Send,
+            onClick = {},
+            enabled = true
         )
     }
 }
@@ -102,9 +125,35 @@ fun TaskInput(
             Text(
                 text = stringResource(label)
             )
-        },
-        modifier = modifier
+        }
     )
+}
+
+@Composable
+fun TaskButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    enabled: Boolean,
+    modifier: Modifier = Modifier
+) {
+    OutlinedIconButton(
+        onClick = onClick,
+        // EDIT THESE LATER/////////////////////////////////////////////////////////////////////////
+        colors = IconButtonColors(
+            containerColor = Color(0xFF005500),
+            contentColor = Color.White,
+            disabledContainerColor = Color.DarkGray,
+            disabledContentColor = Color.LightGray
+        ),
+        enabled = enabled,
+        shape = RoundedCornerShape(3.dp),
+        modifier = Modifier.size(56.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+    }
 }
 
 @Composable
@@ -113,13 +162,42 @@ fun TaskList(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TaskItem(modifier: Modifier = Modifier) {
-
+fun TaskItem(
+    task: Task,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = task.completed,
+                onCheckedChange = { task.completed = it }
+            )
+            Text(
+                text = task.name
+            )
+            Spacer(
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(
+                onClick = onClick
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = stringResource(R.string.delete_button_description)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskManagerPreview() {
     MADLab2TaskManagerTheme {
         MainScreen()
     }
---
+++ app/src/main/java/com/example/madlab2taskmanager/data/Task.kt
@@ -0,0 +1,6 @@
+package com.example.madlab2taskmanager.data
+
+data class Task(
+    val name: String,
+    var completed: Boolean
+)
